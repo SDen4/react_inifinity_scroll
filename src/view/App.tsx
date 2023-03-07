18926331628a4c51b999b3bff6e9e50161e6f673
SDen4 <asw325@yandex.ru>
2023-03-07 20:20:08 +0500

    feat: logic of up button

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Header } from 'components/Header';
 import { Result } from 'components/Result';
 import { Search } from 'components/Search';
 import { Button } from 'components/ui/Button';
 
+import { usersListSelect } from 'selectors/main';
+
 import styles from './styles.css';
 
 export const App = () => {
+  const usersList = useSelector(usersListSelect);
+
+  const onClick = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
   return (
     <main className={styles.main}>
       <Header />
@@ -20,7 +29,9 @@ export const App = () => {
         <Result />
       </section>
 
-      <Button type="button" className={styles.upButton} />
+      {usersList?.length ? (
+        <Button type="button" className={styles.upButton} onClick={onClick} />
+      ) : null}
     </main>
   );
 };
